f170f6cbe8ba6e3ac948c9888da7547dda4bbfff
abdulatifkhan <anvarkhanov96@gmail.com>
2021-03-04 13:40:42 +0500

    Add app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Classified from './Components/Classified/Classified.jsx';
+
+function App () {
+
+  return (
+    <React.Fragment>
+      <Classified />
+    </React.Fragment>
+  )
+}
+
+export default App
