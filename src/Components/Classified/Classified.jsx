3ee0de3fcd8fa8d62d1da394084dfae1cc1ef326
abdulatifkhan <anvarkhanov96@gmail.com>
2021-03-04 13:41:05 +0500

    Add classified.jsx

---
+++ src/Components/Classified/Classified.jsx
@@ -0,0 +1,14 @@
+import { CLASSIFIEDS } from "./Query.js";
+
+console.log(CLASSIFIEDS);
+
+function Classified () {
+
+  return (
+    <>
+      Classified
+    </>
+  )
+}
+
+export default Classified
